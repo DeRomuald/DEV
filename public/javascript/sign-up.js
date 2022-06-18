106ce9163ab77bcafbe7a1674687cc86e71b3d10
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-06-18 16:26:43 -0400

    adding signup form

--- public/javascript/sign-up.js
+++ public/javascript/sign-up.js
@@ -1,4 +1,4 @@
-<form class="signup-page">
+/* <form class="signup-page">
     <div> 
         <label for="username-signup">Username</label>
         <input type="text" id="username-signup"/>
@@ -17,4 +17,27 @@
     <div>
         <button type="submit">Join Our Cult</button>
     </div>
-</form>
+</form> */
+
+function signupFormHandler(event) {
+    event.preventDefault ();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if(username && email && password) {
+        fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify ({
+                username,
+                email,
+                password
+            }),
+
+            headers: {'Content-Type': 'application/json'}
+        }).then((response) => {console.log(response)})
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
