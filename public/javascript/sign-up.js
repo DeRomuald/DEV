a319327e809d32b22152d98448470672f842f75d
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-06-18 16:13:19 -0400

    adding routes and login and signup

---
+++ public/javascript/sign-up.js
@@ -0,0 +1,20 @@
+<form class="signup-page">
+    <div> 
+        <label for="username-signup">Username</label>
+        <input type="text" id="username-signup"/>
+    </div>
+
+    <div> 
+        <label for="email-signup">Email</label>
+        <input type="text" id="email-signup"/>
+    </div>
+
+    <div> 
+        <label for="password-signup">Password</label>
+        <input type="text" id="password-signup"/>
+    </div>
+
+    <div>
+        <button type="submit">Join Our Cult</button>
+    </div>
+</form>
