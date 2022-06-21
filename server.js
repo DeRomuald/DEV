9b08449c68916e9272b746cbe5e71d15ffdc5904
Celine Davilmar <rdavi106@fiu.edu>
2022-06-20 20:03:14 -0400

    completed server page and server functionality

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers/home-routes');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+/* app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+})); */
+
+app.get('/', function (req, res) {
+    res.render('login', { layout: 'index' });
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log('listening on 3001'))
 
