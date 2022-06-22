697c034414434b1090fa1a832a6b097d07e08e7d
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-06-22 11:01:18 -0400

    fixing

--- server.js
+++ server.js
@@ -3,3 +3,27 @@ const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.static('public');
+
+
+app.get('/', function (req, res) {
+    res.render('main', { layout: 'index' });
+})
+
+app.get('/starter', function (req, res) {
+    res.render('starter');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/homepage', function (req, res) {
+    res.render('homepage');
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log('listening on 3001'))
+
